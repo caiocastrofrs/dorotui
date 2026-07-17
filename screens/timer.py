12284b92f71dde781a7468f49e5e1bed8174dbbc
Caio Castro <caiocastrofrs@gmail.com>
2026-07-17 00:47:59 -0300

    refactor: remove unnecessary reactive data

--- screens/timer.py
+++ screens/timer.py
@@ -18,34 +18,22 @@ class TimeDisplay(Digits):
     config = load_config()
 
     time = reactive(0)
-
-    default_time: reactive[int] = reactive(0)
     current_timer = 'focus'
 
     def on_mount(self) -> None:
-        self.time = self.config['default_focus_time'] * 3600
-        if self.current_timer == 'focus':
-            self.update_default_time(self.app.config['default_focus_time'])
-        elif self.current_timer == 'rest':
-            self.update_default_time(self.app.config['default_rest_time'])
+        self.reset()
         self.update_timer = self.set_interval(1/60, self.update_time, pause=True)
-
-    def update_default_time(self, new_time) -> None:
-        self.default_time = new_time
 
 
     def update_time(self) -> None:
         if self.time > 0:
-            self.time -= 1
+            self.time -= 500
         else:
             self.stop()
             self.reset()
             subprocess.run(["paplay","--volume=30000","sounds/alarm-clock-elapsed.oga"])
             self.query_ancestor(TimerScreen).remove_class("started")
             self.toggle_timer()
-
-    def watch_default_time(self) -> None:
-        self.time = self.default_time * 3600
 
     def watch_time(self, time: int) -> None:
         seconds, _ = divmod(time, 60)
@@ -59,18 +47,11 @@ class TimeDisplay(Digits):
         self.update_timer.pause()
 
     def reset(self) -> None:
-        if self.current_timer == 'focus':
-            self.time = self.app.config['default_focus_time'] * 3600
-        elif self.current_timer == 'rest':
-            self.time = self.app.config['default_rest_time'] * 3600
+        self.time = (self.app.config['default_focus_time'] if self.current_timer == 'focus' else self.app.config['default_rest_time']) * 3600
 
     def toggle_timer(self) -> None:
-        if self.current_timer == 'focus':
-            self.current_timer = 'rest'
-            self.time = self.app.config['default_rest_time'] * 3600
-        elif self.current_timer == 'rest':
-            self.current_timer = 'focus'
-            self.time = self.app.config['default_focus_time'] * 3600
+        self.current_timer = ('focus' if self.current_timer == 'rest' else 'rest')
+        self.reset()
 
 class TimerScreen(Screen):
     CSS_PATH="../styles/timer.tcss"
@@ -85,6 +66,7 @@ class TimerScreen(Screen):
         if button_id == "start":
             time_display.start()
             self.add_class("started")
+            self.app.add_class('focus')
         elif button_id == "stop":
             time_display.stop()
             self.remove_class("started")
@@ -92,10 +74,7 @@ class TimerScreen(Screen):
             time_display.reset()
 
     def on_screen_resume(self) -> None:
-        if self.time_display.current_timer == 'focus':
-            self.time_display.update_default_time(self.app.config['default_focus_time'])
-        elif self.time_display.current_timer == 'rest':
-            self.time_display.update_default_time(self.app.config['default_rest_time'])
+        self.time_display.reset()
 
     def compose(self) -> ComposeResult:
         yield CHeader()
