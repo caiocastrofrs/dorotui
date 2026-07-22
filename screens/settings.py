25f8d415bb0054b7e124f4df60be0afd4ee4818f
Caio Castro <caiocastrofrs@gmail.com>
2026-07-22 18:17:33 -0300

    refactor: create a copy of config to make it trigger the watch_config

--- screens/settings.py
+++ screens/settings.py
@@ -5,7 +5,7 @@ from textual.containers import CenterMiddle, HorizontalGroup
 from textual.screen import Screen
 from textual.widgets import Button, Footer, Input, Label
 
-from configuration import DEFAULT_CONFIG, save_config
+from configuration import DEFAULT_CONFIG
 from widgets.header import CHeader
 
 if TYPE_CHECKING:
@@ -32,21 +32,24 @@ class SettingsScreen(Screen):
         yield Footer()
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
-        new_config = self.app.config
+        updated_config = self.app.config.copy()
         if event.button.id == "save":
-            focus_time = self.query_exactly_one("#focus_time", Input).value
-            rest_time = self.query_exactly_one("#rest_time", Input).value
+            focus_time = int(self.query_exactly_one("#focus_time", Input).value)
+            rest_time = int(self.query_exactly_one("#rest_time", Input).value)
 
             if focus_time:
-                new_config["default_focus_time"] = int(focus_time)
+                updated_config["default_focus_time"] = focus_time
                 self.notify(f"Session time set to {focus_time}")
             if rest_time:
-                new_config["default_rest_time"] = int(rest_time)
+                updated_config["default_rest_time"] = rest_time
                 self.notify(f"Rest time set to {rest_time}")
 
+            if not focus_time or not rest_time:
+                self.notify("1 is the minimum allowed value", severity="warning")
+
         if event.button.id == "default":
-            new_config["default_focus_time"] = DEFAULT_CONFIG["default_focus_time"]
-            new_config["default_rest_time"] = DEFAULT_CONFIG["default_rest_time"]
+            updated_config["default_focus_time"] = DEFAULT_CONFIG["default_focus_time"]
+            updated_config["default_rest_time"] = DEFAULT_CONFIG["default_rest_time"]
             self.notify("All settings restored to default.")
 
-        save_config(new_config)
+        self.app.config = updated_config
